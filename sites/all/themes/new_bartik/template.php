2a059032cd76860d504a1aba9c42e495c499ceb8
Francesco Caspani <fcaspani@wellnet.it>
2015-02-18 15:51:28 +0100

    Add function for override breadcrumbs display

--- sites/all/themes/new_bartik/template.php
+++ sites/all/themes/new_bartik/template.php
@@ -164,3 +164,21 @@ function new_bartik_field__taxonomy_term_reference($variables) {
 
   return $output;
 }
+/**
+ * Returns HTML for a breadcrumb trail.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - breadcrumb: An array containing the breadcrumb links.
+ */
+function new_bartik_breadcrumb(&$variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
